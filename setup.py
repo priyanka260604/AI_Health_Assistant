56586c916c881e54e32d51aeac919bec6c9862c0
priyanka260604 <priyanka260604@gmail.com>
2026-08-08 19:36:31 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="AI_Health_Assistant",
+    version="0.1",
+    author="Your Name",
+    author_email="your_email@example.com",
+    packages=find_packages(),
+    install_requires=[]
+)
---
+++ test_logger.py
@@ -0,0 +1,7 @@
+from src.logger import logger
+
+logger.info("Application Started")
+logger.info("Reading Dataset")
+logger.info("Dataset Loaded Successfully")
+logger.warning("This is a warning")
+logger.error("This is an error")
